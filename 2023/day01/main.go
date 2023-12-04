127af56fee1870570ec11cc73fed46cf349b972e
Matthew Wilson <54033231+wilson-matthew@users.noreply.github.com>
2023-12-04 21:29:39 +0000

    Added day 1 part 2

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
-	"unicode"
+	"strings"
 )
 
 func readLines(filePath string) ([]string, error) {
@@ -26,25 +27,73 @@ func readLines(filePath string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func findDigitsInString(word string) []rune {
-	var digitsInWord []rune
+// Function to find first occuring number
+// Iterate over the digits slice
+// Use strings.index(str, substr) to check whether that digit exists, and if it does, at what index
+// Check if digit exists in string
+// Check if it occurs at an earlier index than any of the others
+// If it doesn't, just ignore it
+// If it does, set var firstOccurenceOfDigit to that digit
 
-	// This returns byte position of the character and the character
-	for _, char := range word {
-		if unicode.IsDigit(char) {
-			digitsInWord = append(digitsInWord, char)
+func findFirstDigitInString(str string) string {
+	var firstDigitInString string
+	currentDigitIndex := math.MaxInt16
+	digits := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	for _, digit := range digits {
+		indexOfDigit := strings.Index(str, digit)
+		if indexOfDigit > -1 && indexOfDigit < currentDigitIndex {
+			firstDigitInString = digit
+			currentDigitIndex = indexOfDigit
 		}
 	}
-
-	return digitsInWord
+	return firstDigitInString
 }
 
-func constructCalibrationValue(digitsInWord []rune) int {
-	firstDigitRune := digitsInWord[0]
-	lastDigitRune := digitsInWord[len(digitsInWord)-1]
-	firstDigitString := string(firstDigitRune)
-	lastDigitString := string(lastDigitRune)
-	calibrationValueAsString := firstDigitString + lastDigitString
+func findLastDigitInString(str string) string {
+	var lastDigitInString string
+	currentDigitIndex := -1
+	digits := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	for _, digit := range digits {
+		indexOfDigit := strings.LastIndex(str, digit)
+		if indexOfDigit > -1 && indexOfDigit > currentDigitIndex {
+			lastDigitInString = digit
+			currentDigitIndex = indexOfDigit
+		}
+	}
+	return lastDigitInString
+}
+
+func convertNumbersToDigits(digit string) string {
+	var digitAsInt string
+
+	switch digit {
+	case "1", "one":
+		digitAsInt = "1"
+	case "2", "two":
+		digitAsInt = "2"
+	case "3", "three":
+		digitAsInt = "3"
+	case "4", "four":
+		digitAsInt = "4"
+	case "5", "five":
+		digitAsInt = "5"
+	case "6", "six":
+		digitAsInt = "6"
+	case "7", "seven":
+		digitAsInt = "7"
+	case "8", "eight":
+		digitAsInt = "8"
+	case "9", "nine":
+		digitAsInt = "9"
+	}
+
+	return digitAsInt
+}
+
+func constructCalibrationValue(firstDigit string, lastDigit string) int {
+	calibrationValueAsString := firstDigit + lastDigit
 	calibrationValueAsInt, _ := strconv.Atoi(calibrationValueAsString)
 	return calibrationValueAsInt
 }
@@ -54,8 +103,11 @@ func recoverCalibrationValues(lines []string) int {
 
 	// Two values are returned when ranging over a slice: index, and a copy of the element at that index
 	for _, word := range lines {
-		digitsInWord := findDigitsInString(word)
-		calibrationValue := constructCalibrationValue(digitsInWord)
+		firstDigitInString := findFirstDigitInString(word)
+		lastDigitInString := findLastDigitInString(word)
+		firstDigitAsDigit := convertNumbersToDigits(firstDigitInString)
+		lastDigitAsDigit := convertNumbersToDigits(lastDigitInString)
+		calibrationValue := constructCalibrationValue(firstDigitAsDigit, lastDigitAsDigit)
 		calibrationValueTotal += calibrationValue
 	}
 
